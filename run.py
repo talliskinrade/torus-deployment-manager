840a2fc79efb9e02a7903036219bfd9a96209310
talliskinrade <tallis.kinrade@gmail.com>
2025-07-24 16:44:03 +0100

    corrected spelling error

--- run.py
+++ run.py
@@ -284,7 +284,7 @@ if len(sys.argv) < 2:
     print("Incorrect input parameters. Set HID.")
     sys.exit(1)
 
-config_file = sys.argv[1] + ".yyaml"
+config_file = sys.argv[1] + ".yaml"
 
 if os.path.isfile(join("config", config_file)) == False:
     print("Configuration file does not exist.")
